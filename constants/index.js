047a84bcec7f66d68c4683aa41654c72b3f7fe04
Jeff Elsey <jcelsey@gmail.com>
2022-04-25 15:21:09 -0600

    updated stuff so I can see my nfts on opensea

--- constants/index.js
+++ constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0xB54D92Ec1B51E7C816369bd43c9dCf918ad78841";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://nft-collection-tutorial-blue.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
